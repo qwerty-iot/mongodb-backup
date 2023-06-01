65674a245784d520ecb58d09a1f2372490b18340
Jim Wert <jimwert@gmail.com>
2023-05-31 22:26:17 -0400

    changed to read from primary

--- run.sh
+++ run.sh
@@ -21,7 +21,7 @@ else
   DATE=`date +%Y%m%d-%H%M`
   BACKUPNAME="${BACKUP_PREFIX}-${DATE}.tar.gz"
 
-  mongodump --uri=${MONGO_URI} --db=${MONGO_DB} --authenticationDatabase=admin --readPreference=secondary --gzip --archive="backup.tar.gz"
+  mongodump --uri=${MONGO_URI} --db=${MONGO_DB} --authenticationDatabase=admin --readPreference=primary --gzip --archive="backup.tar.gz"
 
   az storage blob upload --container-name ${AZURE_CONTAINER} -n=${BACKUPNAME} -f ./backup.tar.gz --connection-string="${AZURE_CONNSTRING}"
 fi
